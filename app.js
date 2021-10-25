218823e2fa9896b7ef6d2bb7f640108da4f5ecc3
eklipsorz <eklipsorz@protonmail.ch>
2021-10-25 21:00:29 +0800

    feat: add handlebars settings

--- app.js
+++ app.js
@@ -6,6 +6,27 @@ const app = express()
 // define port
 const port = 3500
 
+// define view engine
+const viewEngine = require('express-handlebars').create({
+  layoutsDir: "views/layouts",
+  extname: ".handlebars",
+  defaultLayout: "main"
+})
+
+// create a engine instance to the express server
+
+// bind the engine to the file type
+app.engine('handlebars', viewEngine.engine)
+
+// set default views path
+app.set('views', process.cwd() + '/views')
+
+// set engine to handlebars
+app.set('view engine', 'handlebars')
+
+
+
+
 app.get('/', (res, req) => {
   req.send('hi, this is a draft')
 })
