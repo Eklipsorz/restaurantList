f1f755e3ec9281781b41b9ff98e721a08a9ea803
eklipsorz <eklipsorz@protonmail.ch>
2021-11-28 22:07:01 +0800

    feat: add image preview function

--- public/javascripts/formValidation.js
+++ public/javascripts/formValidation.js
@@ -1,5 +1,6 @@
-const form = document.querySelector('#form')
+const form = document.querySelector('#add-restaurant-form')
 const submitButton = document.querySelector('#submit')
+const previewImageButton = document.querySelector('#preview-image-btn')
 
 function onSubmitButtonClicked(event) {
   form.classList.add('was-validated')
@@ -10,9 +11,33 @@ function onFormSubmited(event) {
   event.stopPropagation()
 }
 
+function onPreviewImageButtonClicked(event) {
+
+  const imageURL = form.querySelector('#image').value
+  const dialog = document.querySelector('#dialog-content')
+  dialog.innerHTML = ''
+
+  if (imageURL === '') {
+
+    dialog.innerHTML = `
+      <div class="not-found-page">
+        <h1> not found!!!</h1>
+      </div>
+    `
+    return
+  }
+
+
+  dialog.innerHTML = `
+    <img src=${imageURL} alt="" >
+  `
+
+}
+
+
 
 form.addEventListener('submit', onFormSubmited)
 submitButton.addEventListener('click', onSubmitButtonClicked)
-
+previewImageButton.addEventListener('click', onPreviewImageButtonClicked)
 
 
