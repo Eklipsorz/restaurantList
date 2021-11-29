9c7c4700d07f4b910d4b0314381ba44e0e72375d
eklipsorz <eklipsorz@protonmail.ch>
2021-11-29 21:59:38 +0800

    feat: modify search function

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const handlebarsModule = require('express-handlebars')
 const mongoose = require('mongoose')
 const restaurantModel = require('./models/restaurantModel')
 const db = require('./config/connectMongoDB')
-// 
+
 const restaurantList = require('./restaurant.json').results
+
+
 const app = express()
 
 // define engine setting by creating handlebars instance
@@ -16,8 +18,26 @@ const handlebarsInstance = handlebarsModule.create({
   // set default global layout
   defaultLayout: "main",
   // set template file extension to .hbs
-  extname: ".hbs"
+  extname: ".hbs",
+
+  helpers: {
+    alertModel: function (enableAlert, message) {
+      if (!enableAlert) {
+        return ''
+      }
+
+      return `
+        Swal.fire({
+            title:"${message.title}",
+            text:"${message.text}",
+            icon:"error",
+            confirmButtonText:"確認",
+        })
+      `
+    }
+  }
 })
+
 
 // define port
 const port = 3600
@@ -39,16 +59,6 @@ app.use('/', express.static('public'))
 // set body parser for post message
 app.use('/', express.urlencoded({ extended: true }))
 
-// mongoose.connect(`mongodb://localhost:${dbPort}/${dbName}`)
-// const db = mongoose.connection
-
-// db.on('error', () => {
-//   console.log('mongodb error!')
-// })
-
-// db.once('open', async () => {
-//   console.log('mongodb connected!')
-// })
 
 
 // define route for root
@@ -134,34 +144,57 @@ app.post('/restaurants/:id/delete', (req, res) => {
 app.get('/search', (req, res) => {
 
   // fetch keyword and trim additional spaces
-  const keyword = req.query.keyword.trim()
+  const originKeyword = req.query.keyword
+  const keyword = originKeyword.trim().toLowerCase()
 
   if (keyword === '') {
     res.redirect('/')
     return
   }
 
+  restaurantModel.find({
+    $or: [
+      { name: { $regex: keyword, $options: 'i' } },
+      { name_en: { $regex: keyword, $options: 'i' } },
+      { category: { $regex: keyword, $options: 'i' } }
+    ]
+  })
+    .lean()
+    .exec()
+    .then(filteredRestaurants => {
+      let restaurants = filteredRestaurants.length ? filteredRestaurants : restaurantList
+      let enableAlert = filteredRestaurants.length ? false : true
+      let message = {
+        title: '搜尋失敗',
+        text: `找不到名為 ${originKeyword} 的餐廳，請更換名稱`
+      }
+
+      res.render('index', { restaurants, enableAlert, message })
+    })
+    .catch(error => console.log(error))
+
+
 
   // find a set of restaurants by keyword and put them into filteredRestaurants
   // filteredRestaurants represents search result
-  const filteredRestaurants = restaurantList.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
-      restaurant.category.toLowerCase().includes(keyword.toLowerCase())
-  })
+  // const filteredRestaurants = restaurantList.filter(restaurant => {
+  //   return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
+  //     restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+  // })
 
 
   // if filteredRestaurants is empty, the system will render to origin view with a alert widget
   // if filteredRestaurants is not empty, the system will render to new view via search result
-  let restaurants = filteredRestaurants.length ? filteredRestaurants : restaurantList
+  // let restaurants = filteredRestaurants.length ? filteredRestaurants : restaurantList
 
   // enable alert widget to remind user.
   // true means a alert tell us the system find nothing by keyword.
   // false means nothing happened .
-  let enableAlert = filteredRestaurants.length ? false : true
+  // let enableAlert = filteredRestaurants.length ? false : true
 
 
   // render with index.hbs, search results, keyword, enableAlert which enables alert widget to remind user
-  res.render('index', { restaurants, keyword, enableAlert })
+  // res.render('index', { restaurants, keyword, enableAlert })
 })
 
 
