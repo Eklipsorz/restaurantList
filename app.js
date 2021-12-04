01383eaf9a9d9bf0867adc8cf01a9e9e7fb72328
eklipsorz <eklipsorz@protonmail.ch>
2021-12-04 14:45:10 +0800

    refaceor: move / and /search into routes module

--- app.js
+++ app.js
@@ -76,29 +76,15 @@ app.use('/', express.urlencoded({ extended: true }))
 
 app.use('/', methodOverride('_method'))
 
-
+app.use('/', router)
 // each query is not promise, so it need to add query.exec() or query.then()
 // however, query.then() is also not really then in promise and each then()
 // execute the query. These can execute the query multiple times. Therefore, 
 // I chose query.exec() to transfer query to a real promise and use it's 
 // then and catch syntax.
 
-// define route for root
-app.get('/', (req, res) => {
 
-  // turn off alert model
-  let enableAlert = false
 
-  // find all restaurants and render
-  restaurantModel.find({})
-    .lean()
-    .exec()
-    .then(restaurants => {
-      restaurantList = restaurants
-      res.render('index', { restaurants, enableAlert })
-    })
-    .catch(error => console.log(error))
-})
 
 // define route for creation page
 app.get('/restaurants/new', (req, res) => {
@@ -149,7 +135,6 @@ app.get('/restaurants/:id/edit', (req, res) => {
 
 // define route for editing restaurant data on edit page
 app.put('/restaurants/:id', (req, res) => {
-  console.log('this is update')
   const reqId = req.params.id
   const targetRestaurant = req.body
 
@@ -163,7 +148,7 @@ app.put('/restaurants/:id', (req, res) => {
 // define route for deleting a restaurant
 app.delete('/restaurants/:id', (req, res) => {
 
-  console.log('this is delete')
+
   const reqId = req.params.id
 
   // find the restaurant by id and delete it
@@ -173,50 +158,6 @@ app.delete('/restaurants/:id', (req, res) => {
     .catch((error) => console.log(error))
 })
 
-
-
-// define route for searching
-app.get('/search', (req, res) => {
-
-  // get origin keyword for showing message on the alert model
-  const originKeyword = req.query.keyword
-  // fetch keyword and trim additional spaces
-  const keyword = originKeyword.trim().toLowerCase()
-
-
-  // if user input nothing
-  if (keyword === '') {
-    res.redirect('/')
-    return
-  }
-
-  // if user input something, it try to find the restaurant with three fields
-  // (name, name_en, category) and get the search result (called filteredRestaurants)
-  restaurantModel.find({
-    $or: [
-      { name: { $regex: keyword, $options: 'i' } },
-      { category: { $regex: keyword, $options: 'i' } }
-    ]
-  })
-    .lean()
-    .exec()
-    .then(filteredRestaurants => {
-      // if filteredRestaurants is empty, the system will render to origin view with a alert widget
-      // if filteredRestaurants is not empty, the system will render to new view via search result
-      let restaurants = filteredRestaurants.length ? filteredRestaurants : restaurantList
-      // turn on alert model
-      let enableAlert = filteredRestaurants.length ? false : true
-      // define the message on alert model
-      let message = {
-        title: '搜尋失敗',
-        text: `找不到名為 ${originKeyword} 的餐廳，請更換名稱`
-      }
-      // render a page according search results
-      res.render('index', { restaurants, enableAlert, message })
-    })
-    .catch(error => console.log(error))
-
-})
 
 
 // start to listening at port 3500
--- routes/modules/root.js
+++ routes/modules/root.js
@@ -4,8 +4,66 @@ const restaurantModel = require('../../models/restaurantModel')
 
 const router = express.Router()
 
+// define route for root
+router.get('/', (req, res) => {
+
+  // turn off alert model
+  let enableAlert = false
+
+  // find all restaurants and render
+  restaurantModel.find({})
+    .lean()
+    .exec()
+    .then(restaurants => {
+      restaurantList = restaurants
+      res.render('index', { restaurants, enableAlert })
+    })
+    .catch(error => console.log(error))
+})
 
 
 
+// define route for searching
+router.get('/search', (req, res) => {
+
+  // get origin keyword for showing message on the alert model
+  const originKeyword = req.query.keyword
+  // fetch keyword and trim additional spaces
+  const keyword = originKeyword.trim().toLowerCase()
+
+
+  // if user input nothing
+  if (keyword === '') {
+    res.redirect('/')
+    return
+  }
+
+  // if user input something, it try to find the restaurant with three fields
+  // (name, name_en, category) and get the search result (called filteredRestaurants)
+  restaurantModel.find({
+    $or: [
+      { name: { $regex: keyword, $options: 'i' } },
+      { category: { $regex: keyword, $options: 'i' } }
+    ]
+  })
+    .lean()
+    .exec()
+    .then(filteredRestaurants => {
+      // if filteredRestaurants is empty, the system will render to origin view with a alert widget
+      // if filteredRestaurants is not empty, the system will render to new view via search result
+      let restaurants = filteredRestaurants.length ? filteredRestaurants : restaurantList
+      // turn on alert model
+      let enableAlert = filteredRestaurants.length ? false : true
+      // define the message on alert model
+      let message = {
+        title: '搜尋失敗',
+        text: `找不到名為 ${originKeyword} 的餐廳，請更換名稱`
+      }
+      // render a page according search results
+      res.render('index', { restaurants, enableAlert, message })
+    })
+    .catch(error => console.log(error))
+
+})
 
 module.exports = router
