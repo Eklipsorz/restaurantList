e2c94d010f08a2a4a9664079e11fc799837c0453
eklipsorz <eklipsorz@protonmail.ch>
2021-12-30 19:05:23 +0800

    refactor: rename to entryLayout.hbs

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 
 router.get('/register', (req, res) => {
-  res.render('register', { layout: 'register' })
+  res.render('register', { layout: 'entryLayout' })
 })
 
 router.post('/register', (req, res) => {
